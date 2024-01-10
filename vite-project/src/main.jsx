1189107abd6e794f86ac8cfa41f3364c877614d7
Hazel Windle <hwindle83@googlemail.com>
2024-01-10 15:40:14 +0000

    installed theme-ui from @emotion/react

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { ThemeUIProvider } from 'theme-ui';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeUIProvider>
+      <App />
+    </ThemeUIProvider>
+  </React.StrictMode>
+);
