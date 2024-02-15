81557a2f24d47856c2400fbe391b47ad24f10e05
Hazel Windle <hwindle83@googlemail.com>
2024-02-15 18:45:51 +0000

    Added dummy data for when Postgresql is not running.

--- vite-project/src/containers/overview/Overview.jsx
+++ vite-project/src/containers/overview/Overview.jsx
@@ -1,35 +1,42 @@
 import OverviewCard from '../../components/OverviewCard/OverviewCard';
 import './Overview.css';
-import { useQuery } from '@tanstack/react-query';
+// import { useQuery } from '@tanstack/react-query';
 
 function Overview() {
-  const { isLoading, error, data } = useQuery({
-    queryKey: ['overviewData'],
-    queryFn: () =>
-      fetch('http://localhost:3001/overview').then((res) =>
-        res.json()
-      )
-  })
+  const data = [
+    { type: 'facebook', title: 'Page Views', number: 52, percentage: -2 },
+    { type: 'facebook', title: 'Likes', number: 52, percentage: 3 },
+    { type: 'instagram', title: 'Likes', number: 5264, percentage: 2257 },
+    {
+      type: 'instagram',
+      title: 'Profile Views',
+      number: 52000,
+      percentage: 1375,
+    },
+    { type: 'twitter', title: 'Retweets', number: 117, percentage: 303 },
+    { type: 'twitter', title: 'Likes', number: 507, percentage: 553 },
+    { type: 'youtube', title: 'Likes', number: 107, percentage: -19 },
+    { type: 'youtube', title: 'Total Views', number: 1407, percentage: -12 },
+  ];
 
-  if (isLoading) return 'Loading...';
+  // const { isLoading, error, data } = useQuery({
+  //   queryKey: ['overviewData'],
+  //   queryFn: () =>
+  //     fetch('http://localhost:3001/overview').then((res) =>
+  //       res.json()
+  //     )
+  // })
 
-  if (error) return 'An error has occurred: ' + error.message;
+  // if (isLoading) return 'Loading...';
+
+  // if (error) return 'An error has occurred: ' + error.message;
 
   return (
-    
-      <section className='overview-area'>
-        {data.map((row, index) => {
-          return <OverviewCard data={row} key={index} />;
-        })}
-        {/* <OverviewCard type='facebook' title='Page Views' number='87' percentage='3' />
-      <OverviewCard type='facebook' title='Likes' number='52' percentage='-2' />
-      <OverviewCard type='instagram' title='Likes' number='5264' percentage='2257' />
-      <OverviewCard type='instagram' title='Profile Views' number='52k' percentage='1375' />
-      <OverviewCard type='twitter' title='Retweets' number='117' percentage='303' />
-      <OverviewCard type='twitter' title='Likes' number='507' percentage='553' />
-      <OverviewCard type='youtube' title='Likes' number='107' percentage='-19' />
-      <OverviewCard type='youtube' title='Total Views' number='1407' percentage='-12' />       */}
-      </section>
+    <section className='overview-area'>
+      {data.map((row, index) => {
+        return <OverviewCard data={row} key={index} />;
+      })}
+    </section>
   );
 }
 
--- vite-project/src/containers/top-area-followers/TopFollowers.jsx
+++ vite-project/src/containers/top-area-followers/TopFollowers.jsx
@@ -1,19 +1,25 @@
 import FollowerCard from '../../components/FollowerCard/FollowerCard';
 import './TopFollowers.css';
-import { useQuery } from '@tanstack/react-query';
+// import { useQuery } from '@tanstack/react-query';
 
 function TopFollowers() {
-  const { isLoading, error, data } = useQuery({
-    queryKey: ['followersData'],
-    queryFn: () =>
-      fetch('http://localhost:3001/topfollowers').then((res) =>
-        res.json()
-      )
-  })
+  const data = [
+    { type: 'facebook', handle: '@nathanf', followers: 1987, trend: 12 },
+    { type: 'twitter', handle: '@nathanf', followers: 1044, trend: 99 },
+    { type: 'instagram', handle: '@realnathanf', followers: 11000, trend: 1099 },
+    { type: 'youtube', handle: 'Nathan F.', followers: 8239, trend: -144 }
+  ];
+  // const { isLoading, error, data } = useQuery({
+  //   queryKey: ['followersData'],
+  //   queryFn: () =>
+  //     fetch('http://localhost:3001/topfollowers').then((res) =>
+  //       res.json()
+  //     )
+  // })
 
-  if (isLoading) return 'Loading...';
+  // if (isLoading) return 'Loading...';
 
-  if (error) return 'An error has occurred: ' + error.message;
+  // if (error) return 'An error has occurred: ' + error.message;
 
   return (
     <section className='top-followers'>
